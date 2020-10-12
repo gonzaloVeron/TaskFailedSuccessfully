3de859ca6ec61884f228e0441da8934ce954a8b5
martinmeza12 <martinmeza476@gmail.com>
2020-10-11 22:48:43 -0300

    Animacion de cambio de niveles terminada

--- objects/exit/Exit.gd
+++ objects/exit/Exit.gd
@@ -1,18 +1,26 @@
 extends Area2D
 
-var next_level_scene_path : String = ""
+#var next_level_scene_path : String = ""
+export var next_scene : PackedScene
 
+onready var anim_player : AnimationPlayer = $AnimationPlayer
 
-func set_next_level_scene_path (scene_path):
-	next_level_scene_path = scene_path
+#func set_next_level_scene_path (scene_path):
+#	next_level_scene_path = scene_path
 
 func _ready():
-	$AnimationPlayer.play("default")
+	#$AnimationPlayer.play("default")
+	anim_player.play("default")
 	pass 
 
+func teleport():
+	anim_player.play("fade_in")
+	yield(anim_player,"animation_finished")
+	get_tree().change_scene_to(next_scene)
 
 func _on_Exit_body_entered(body):
 	if body.is_in_group("player"):
-		print("se deberia producir el cambio de nivel")
+		teleport()
+		#print("se deberia producir el cambio de nivel")
 		#get_tree().change_scene(next_level_scene_path)
 	
