d5d4868227306557b2a470ff1a410ad01d1a5d6a
Eric Arnez <ericarnez@gmail.com>
2020-10-20 13:06:44 -0300

    añadido de cursor custom

---
+++ UI/Cursor.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	#var arrow = get_node("SpriteCrosshair").texture
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+func _physics_process(delta):
+	#se puede tener el crosshair titileando o el estatico
+	#$SpriteCrosshair.set_global_position(get_global_mouse_position())
+	$AnimatedSprite.set_global_position(get_global_mouse_position())
