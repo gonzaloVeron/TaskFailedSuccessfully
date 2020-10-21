1500805059b9c784bbe5fe6643f9854f3af5e0fa
GonzaloVeron <gonveron96@gmail.com>
2020-10-21 01:06:08 -0300

    Agrego LevelTimer

---
+++ LevelTimer.gd
@@ -0,0 +1,24 @@
+extends Node
+
+signal timerChange(val)
+
+var value = 60
+
+func _ready():
+	emit_signal("timerChange", value)
+
+func _process(delta):
+	value -= delta
+	emit_signal("timerChange", _roundToInt(value))
+
+# --------------------------------------------------- #
+
+func increaseTimer(val):
+	value += val
+	emit_signal("timerChange", _roundToInt(value))
+
+func _roundToInt(val):
+	return int(round(val))
+
+func setTimer(val):
+	value = val
---
+++ LevelTimer/TimerLabel.gd
@@ -0,0 +1,4 @@
+extends Label
+
+func _on_LevelTimer_timerChange(val):
+	text = str(val)
